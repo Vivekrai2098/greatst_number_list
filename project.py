636e9ab5959b321e4b926e41cd7198a55e06c3ee
VIVEK RAI <94076044+Vivekrai2098@users.noreply.github.com>
2022-03-23 13:12:35 +0530

    greatest number

---
+++ project.py
@@ -0,0 +1,7 @@
+l=[]
+print("your list is empty....")
+n=eval(input("how many number list you want"))
+for i in range(n):
+    l.append(i)
+print(l)
+print("this is the largest number in the list",max(l))
